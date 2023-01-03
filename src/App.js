86e7a69b58d2d3eb214c666bcb01d57b22a12e44
Shubham V.  Umap <shubhamvumap@gmail.com>
2023-01-03 21:12:23 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import DataTable from './component/DataTable';
+
+function App() {
+  return (
+    <div className="App">
+      <DataTable />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/DataTable.js
@@ -0,0 +1,48 @@
+import React, { useState, useEffect } from 'react'
+import { DataGrid } from '@material-ui/data-grid'
+
+const columns = [
+  { field: 'warehouse_id', headerName: 'warehouse_id',width: 300 },
+  { field: 'subwarehouse_id', headerName: 'subwarehouse_id',width: 300 },
+  { field: 'warehouse_name', headerName: 'warehouse_name', width: 300 },
+  { field: 'subwarehouse_name', headerName: 'subwarehouse_name', width: 600 }
+]
+
+const DataTable = () => {
+
+  const [tableData, setTableData] = useState([])
+
+  const [rows, setRows] = useState(tableData);
+  const [deletedRows, setDeletedRows] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/warehouse")
+      .then((data) => data.json())
+      .then((data) => setTableData(data.Warehouse))
+.catch((err)=>console.log(err))
+  }, [])
+
+  console.log("tableData",tableData);
+
+  return (
+    <div style={{ height: 700, width: '100%' }}>
+      <h1>Warehouse Data</h1>
+      <DataGrid
+        rows={tableData}
+        columns={columns}
+        getRowId={(row) =>  row._id }
+        pageSize={12}
+        checkboxSelection
+        onSelectionModelChange={({ selectionModel }) => {
+          {console.log(selectionModel)}
+          const rowIds = selectionModel.map(rowId => parseInt(String(rowId), 10));
+          const rowsToDelete = tableData.filter(row => rowIds.includes(row.id));
+          setDeletedRows(rowsToDelete);
+          console.log(deletedRows);
+        }}
+      />
+    </div>
+  )
+}
+
+export default DataTable
